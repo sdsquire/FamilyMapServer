273de6671e28ff35de65f9b8a798ea5e9903b8ea
sdsquire <sdsquires@gmail.com>
2022-02-10 17:50:02 -0700

    Created skeleton of Model Classes and added sample DAO classes

---
+++ src/main/java/DAOs/DataAccessException.java
@@ -0,0 +1,13 @@
+package DAOs;
+
+public class DataAccessException extends Exception {
+    DataAccessException(String message)
+    {
+        super(message);
+    }
+
+    DataAccessException()
+    {
+        super();
+    }
+}
